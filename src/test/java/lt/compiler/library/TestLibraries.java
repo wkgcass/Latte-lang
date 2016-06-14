3e1526f52e2db729c3e6a92570872ade6376b366
K.G. Wang <wkgcass@hotmail.com>
2016-06-14 22:59:43 +0800

    html escape

--- src/test/java/lt/compiler/library/TestLibraries.java
+++ src/test/java/lt/compiler/library/TestLibraries.java
@@ -66,14 +66,14 @@ public class TestLibraries {
                         "</head>" +
                         "<body>" +
                         "<form>" +
-                        "<input value='hello world' type='text'>" +
+                        "<input value='hello&nbsp;world' type='text'>" +
                         "</form>" +
                         "</body>" +
                         "</html>", testHtmlFormat.invoke(null).toString());
 
                 Method testHtmlEscape = TestHtml.getMethod("testHtmlEscape");
                 assertEquals("" +
-                                "<html onload='\\\\\\\"\\'\\n\\r\\t'>" +
+                                "<html onload='window.location.href=&quot;page&quot;'>" +
                                 "&nbsp;&gt;&lt;&amp;&quot;" +
                                 "</html>"
                         , testHtmlEscape.invoke(null).toString());
