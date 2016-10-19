d5f0a9d5528468587272bda5d2148602c17ee624
K.G. Wang <wkgcass@hotmail.com>
2016-10-20 00:24:04 +0800

    win launch script
    still not good enough, but since i rarely use windows (as a result i
    don't know how to write .bat), and it's ok in most cases, i will not
    maintain the win script.

--- latte-build/src/main/java/org/lattelang/compiler/build/ShellCommandGenerator.java
+++ latte-build/src/main/java/org/lattelang/compiler/build/ShellCommandGenerator.java
@@ -16,29 +16,20 @@ public class ShellCommandGenerator {
 
                 String fullName = "latte-build-" + version + "-jar-with-dependencies.jar";
 
-                String bat = "" +
-                        "@echo off\n" +
-                        "\n" +
-                        "setlocal EnableDelayedExpansion\n" +
-                        "\n" +
-                        ":getArg\n" +
-                        "if \"%1%\"==\"\" goto end\n" +
-                        "set input=!input! %1%\n" +
-                        "shift\n" +
-                        "goto getArg\n" +
-                        ":end\n" +
-                        "\n" +
-                        "java -jar \"" + jarDir + "\\" + fullName + "\" !input!\n" +
-                        "\n" +
-                        "endlocal EnableDelayedExpansion";
-
                 File latte_bat = new File(output + File.separator + "latte.bat");
                 if (!latte_bat.exists()) {
                         //noinspection ResultOfMethodCallIgnored
                         latte_bat.createNewFile();
                 }
                 FileOutputStream latte_bat_fos = new FileOutputStream(latte_bat);
-                latte_bat_fos.write(bat.getBytes());
+                InputStream latte_bat_is = ShellCommandGenerator.class.getClassLoader().getResourceAsStream("latte.bat.template");
+                latte_bat_fos.write("@echo off\n".getBytes());
+                latte_bat_fos.write(("set LATTE_JAR=" + jarDir + File.separator + fullName + "\n").getBytes());
+                byte[] buf = new byte[1024];
+                int c;
+                while (-1 != (c = latte_bat_is.read(buf))) {
+                        latte_bat_fos.write(buf, 0, c);
+                }
                 latte_bat_fos.flush();
                 latte_bat_fos.close();
 
@@ -51,8 +42,7 @@ public class ShellCommandGenerator {
                 InputStream latte_sh_is = ShellCommandGenerator.class.getClassLoader().getResourceAsStream("latte.sh.template");
                 latte_sh_fos.write("#!/bin/bash\n".getBytes());
                 latte_sh_fos.write(("LATTE_JAR=\"" + jarDir + File.separator + fullName + "\"\n").getBytes());
-                byte[] buf = new byte[1024];
-                int c;
+                buf = new byte[1024];
                 while (-1 != (c = latte_sh_is.read(buf))) {
                         latte_sh_fos.write(buf, 0, c);
                 }
