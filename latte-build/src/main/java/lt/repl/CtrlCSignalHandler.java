ea961a5ad7d47b3537cee14eaaab3bc42e575ead
K.G. Wang <wkgcass@hotmail.com>
2017-10-05 23:19:33 +0800

    change the double ctrl-c exit delay to 1 second

--- latte-build/src/main/java/lt/repl/CtrlCSignalHandler.java
+++ latte-build/src/main/java/lt/repl/CtrlCSignalHandler.java
@@ -32,7 +32,7 @@ public class CtrlCSignalHandler implements CtrlCHandler {
                                                 @Override
                                                 public void run() {
                                                         try {
-                                                                Thread.sleep(500);
+                                                                Thread.sleep(1000);
                                                         } catch (InterruptedException e) {
                                                                 e.printStackTrace();
                                                         }
